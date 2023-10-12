9a802e650e3a8df62c4f829ac36130a491aa9c33
Camila <cardenascastrolauracamila7@gmail.com>
2023-10-12 13:16:40 -0500

    feat: New logica to edit the content

--- src/components/edit-button.js
+++ src/components/edit-button.js
@@ -1,7 +1,31 @@
-export default function EditButton(){
-    return(
-        <>
-            <button type="button" className="btn btn-lg btn-light">Edit</button>
-        </>
-    )
-}
+import React, { useState } from 'react';
+
+export default function EditButton(props){
+
+      const [isEditing, setIsEditing] = useState(false);
+      const [editedProduct, setEditedProduct] = useState(props.product);
+    
+      function handleEditClick() {
+        setIsEditing(!isEditing);
+        if (!isEditing) {
+          props.onEdit(props.product);
+        }
+      }
+    
+      function saveChanges() {
+        props.onEdit(editedProduct);
+        setIsEditing(false);
+      }
+    
+      return (
+        <div>
+          {isEditing ? (
+            <>
+              <button onClick={saveChanges}>Guardar</button>
+            </>
+          ) : (
+            <button onClick={handleEditClick}>Editar</button>
+          )}
+        </div>
+      );
+    }    
--- src/components/product-list.js
+++ src/components/product-list.js
@@ -1,53 +1,93 @@
-import React from "react";
-import EditButton from "./edit-button";
+import React, { useState } from 'react';
+import EditButton from './edit-button';
 
 export default function ProductList({ products, search }) {
-    function filterProductsBySearch(products, searchTerm) {
-        return products.filter((product) =>
-            product.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-    }
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedProduct, setEditedProduct] = useState(null);
 
-    const filteredProducts = filterProductsBySearch(products, search);
-
-    return (
-        <div className="container">
-  <div className="row">
-    <div className="col-md-12">
-      <table className="table table-bordered table-striped table-hover custom-table">
-        <thead className="bg-primary text-white">
-          <tr>
-            <th>Product</th>
-            <th>Details</th>
-            <th>Price</th>
-            <th>Stock</th>
-            <th>Sku</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredProducts.map((product) => (
-            <tr key={product.id}>
-              <td>
-                <img src={product.image} className="img-fluid product-image" alt={product.name} />
-              </td>
-              <td>
-                <h4>{product.name}</h4>
-                <p>{product.description}</p>
-              </td>
-              <td>${product.price}</td>
-              <td>{product.stock}</td>
-              <td>{product.sku}</td>
-              <td>
-                <EditButton />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  </div>
-</div>
-
+  function filterProductsBySearch(products, searchTerm) {
+    return products.filter((product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+  }
+
+  const filteredProducts = filterProductsBySearch(products, search);
+
+  const handleEditClick = (product) => {
+    setIsEditing(!isEditing);
+    setEditedProduct(product);
+  };
+
+  const saveChanges = () => {
+    // Lógica para guardar los cambios
+    setIsEditing(false);
+    setEditedProduct(null);
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <table className="table table-bordered table-striped table-hover custom-table">
+            <thead className="bg-primary text-white">
+              <tr>
+                <th>Product</th>
+                <th>Details</th>
+                <th>Price</th>
+                <th>Stock</th>
+                <th>Sku</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredProducts.map((product) => (
+                <tr key={product.id}>
+                  <td>
+                    <img src={product.image} className="img-fluid product-image" alt={product.name} />
+                  </td>
+                  <td>
+                    {isEditing && editedProduct && editedProduct.id === product.id ? (
+                      <input
+                        type="text"
+                        value={editedProduct.name}
+                      />
+                    ) : (
+                      <>
+                        <h4>{product.name}</h4>
+                        <p>{product.description}</p>
+                      </>
+                    )}
+                  </td>
+                  <td>${isEditing && editedProduct && editedProduct.id === product.id ? (
+                    <input
+                      type="number"
+                      value={editedProduct.price}
+                    />
+                  ) : (
+                    product.price
+                  )}</td>
+                  <td>{isEditing && editedProduct && editedProduct.id === product.id ? (
+                    <input
+                      type="number"
+                      value={editedProduct.stock}
+                    />
+                  ) : (
+                    product.stock
+                  )}</td>
+                  <td>{product.sku}</td>
+                  <td>
+                    {isEditing && editedProduct && editedProduct.id === product.id ? (
+                      <button onClick={saveChanges}>Guardar</button>
+                    ) : (
+                      <EditButton product={product} onEdit={handleEditClick} />
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
